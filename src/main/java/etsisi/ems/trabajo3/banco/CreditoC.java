cfc283dda6bce7de23b2bdecbdd2f67dcbe7c9ae
Jermaniac <stalin_bnk@hotmail.com>
2019-05-07 19:17:33 +0200

    fitfh commit

---
+++ src/main/java/etsisi/ems/trabajo3/banco/CreditoC.java
@@ -0,0 +1,14 @@
+package etsisi.ems.trabajo3.banco;
+
+import java.util.Date;
+import java.util.Vector;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class CreditoC extends Credito {
+
+	public CreditoC(String numero, String titular, LocalDate fechacaducidad, double credito, int marcainternacional, String nombreentidad, int ccv) {
+		super(numero,titular,fechacaducidad,0,marcainternacional,nombreentidad,ccv);
+		mCredito = credito;
+	}
+}
